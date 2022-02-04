7989a1f1cae9567458c8e57e0e98f2755406e06c
vupha <vupham129@gmail.com>
2022-02-04 11:41:22 -0800

    day2_locators_getText_getAttribute

--- src/test/java/com/cydeo/tests/day2_locators_getText_getAttribute/T3_GoogleSearch.java
+++ src/test/java/com/cydeo/tests/day2_locators_getText_getAttribute/T3_GoogleSearch.java
@@ -2,6 +2,7 @@ package com.cydeo.tests.day2_locators_getText_getAttribute;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -19,11 +20,19 @@ public class T3_GoogleSearch {
 
         //  3- Write “apple” in search box
         WebElement searchBox = driver.findElement(By.name("q"));
-        searchBox.sendKeys("apple");
+        searchBox.sendKeys("apple" + Keys.ENTER);
+        //Keys.ENTER: will press enter after the search
 
+
+//        System.out.println("searchBox.getTagName() = " + searchBox.getTagName());
+//        System.out.println(searchBox.getAttribute("class"));
+//        System.out.println(searchBox.getAttribute("type"));
+//        System.out.println(searchBox.getAttribute("maxLength"));
+//        System.out.println(searchBox.getAttribute("title"));
+//        System.out.println(searchBox.getAttribute("value"));
         // 4- Click google search button
-        WebElement searchButton = driver.findElement(By.name("btnK"));
-        searchButton.submit();
+       // WebElement searchButton = driver.findElement(By.name("btnK"));
+        //searchButton.submit();
 
         // 5- Verify title:
         // Expected: Title should start with “apple” word
