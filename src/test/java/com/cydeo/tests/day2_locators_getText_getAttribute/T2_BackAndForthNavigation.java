3e9520212dc85286396ea0a76fc917405bfb3149
vupha <vupham129@gmail.com>
2022-02-04 11:13:43 -0800

    day2_locators_getText_getAttribute

---
+++ src/test/java/com/cydeo/tests/day2_locators_getText_getAttribute/T2_BackAndForthNavigation.java
@@ -0,0 +1,72 @@
+package com.cydeo.tests.day2_locators_getText_getAttribute;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class T2_BackAndForthNavigation {
+    public static void main(String[] args) throws InterruptedException {
+
+        /*
+        TC #3: Back and forth navigation
+1- Open a chrome browser
+2- Go to: https://practice.cydeo.com
+3- Click to A/B Testing from top of the list.
+4- Verify title is:
+Expected: No A/B Test
+5- Go back to home page by using the .back();
+6- Verify title equals:
+Expected: Practice
+         */
+
+        //1- Open a chrome browser
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+
+        //2- Go to: https://practice.cydeo.com
+        driver.get("https://practice.cydeo.com");
+
+         // 3- Click to A/B Testing from top of the list.
+        WebElement linkText = driver.findElement(By.linkText("A/B Testing"));
+        linkText.click();
+
+         //4- Verify title is:
+        // Expected: No A/B Test
+
+        String expectedTitle = "No A/B Test";
+        String actualTitle = driver.getTitle();
+
+        if(actualTitle.equals(expectedTitle)){
+            System.out.println("Title verification PASSED!");
+        }else{
+            System.out.println("Title verification FAILED!");
+        }
+
+        // stop for 6 seconds to see the title
+        Thread.sleep(6000);
+
+        //5- Go back to home page by using the .back();
+        driver.navigate().back();
+
+        // 6- Verify title equals:
+        // Expected: Practice
+        String expectedTitle1 = "Practice";
+        String actualTitle1 = driver.getTitle();
+
+        if(actualTitle1.equals(expectedTitle1)){
+            System.out.println("Title verification Passed!");
+        }else{
+            System.out.println("Title verification Failed!");
+        }
+
+        driver.close();
+
+
+
+
+
+    }
+}
---
+++ src/test/java/com/cydeo/tests/day2_locators_getText_getAttribute/T3_GoogleSearch.java
@@ -0,0 +1,44 @@
+package com.cydeo.tests.day2_locators_getText_getAttribute;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class T3_GoogleSearch {
+    public static void main(String[] args) {
+        //  TC#3: Google search
+        // 1- Open a chrome browser
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+
+        //2- Go to: https://google.com
+        driver.get("https://google.com");
+
+        //  3- Write “apple” in search box
+        WebElement searchBox = driver.findElement(By.name("q"));
+        searchBox.sendKeys("apple");
+
+        // 4- Click google search button
+        WebElement searchButton = driver.findElement(By.name("btnK"));
+        searchButton.submit();
+
+        // 5- Verify title:
+        // Expected: Title should start with “apple” word
+
+        String expectedTitle = "apple";
+        String actualTitle = driver.getTitle();
+
+        if(actualTitle.startsWith(expectedTitle)){
+            System.out.println("Title verification PASSED!");
+        }else{
+            System.out.println("Title verification FAILED!");
+        }
+
+        driver.close();
+
+
+    }
+}
