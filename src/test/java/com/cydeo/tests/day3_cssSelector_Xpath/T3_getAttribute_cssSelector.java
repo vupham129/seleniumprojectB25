a7990350c374acc5223c0908dec04d351423f7ac
vupha <vupham129@gmail.com>
2022-02-08 12:39:18 -0800

    day3_cssSelector_Xpath

---
+++ src/test/java/com/cydeo/tests/day3_cssSelector_Xpath/T3_getAttribute_cssSelector.java
@@ -0,0 +1,37 @@
+package com.cydeo.tests.day3_cssSelector_Xpath;
+
+import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class T3_getAttribute_cssSelector {
+    public static void main(String[] args) {
+//       TC #3: NextBaseCRM, locators, getText(), getAttribute() practice
+//        1- Open a chrome browser
+        WebDriver driver = WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+
+//        2- Go to: https://login1.nextbasecrm.com/
+        driver.get("https://login1.nextbasecrm.com/");
+
+//        3- Verify “Log in” button text is as expected:
+//        Expected: Log In
+       // WebElement signInButton = driver.findElement(By.className("login-btn"));
+      //  WebElement signInButton = driver.findElement(By.cssSelector("input[class='login-btn']"));
+       // WebElement signInButton = driver.findElement(By.cssSelector("input[type='submit']"));
+        WebElement signInButton = driver.findElement(By.cssSelector("input[value='Log In']"));
+
+        String expectedButtonText = "Log In";
+        String actualButtonText = signInButton.getAttribute("value");
+
+        if(actualButtonText.equals(expectedButtonText)){
+            System.out.println("Log in button text verification passed");
+        }else{
+            System.out.println("Log in button text verification failed");
+        }
+
+
+
+    }
+}
