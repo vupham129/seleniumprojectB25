f4c0d57577c2d1b1d7726daa35ec5eb4e4dbc91c
vupha <vupham129@gmail.com>
2022-02-11 12:13:16 -0800

    day4_findElements_checkboxes_radio

---
+++ src/test/java/com/cydeo/tests/day4_findElements_checkboxes_radios/T4_FindElements.java
@@ -0,0 +1,42 @@
+package com.cydeo.tests.day4_findElements_checkboxes_radios;
+
+import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+public class T4_FindElements {
+    public static void main(String[] args) {
+        //TC #4: FindElements Task
+        //1- Open a Chrome browser
+        WebDriver driver = WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+        //2- Go to: https://practice.cydeo.com/abtest
+        driver.get("https://practice.cydeo.com/abtest");
+
+        //3- Locate all the links in the page.
+        List<WebElement> allLinks = driver.findElements(By.tagName("a"));
+
+        //4- Print out the number of the links on the page.
+        System.out.println("allLinks.size() = " + allLinks.size());
+
+        //5- Print out the texts of the links.
+        //6- Print out the HREF attribute values of the links
+        for (WebElement link : allLinks) {
+            System.out.println("Text of link: " + link.getText());
+            System.out.println("HREF values: " + link.getAttribute("href"));
+        }
+
+        //6- Print out the HREF attribute values of the links
+//        for (WebElement link : allLinks) {
+//            System.out.println("HREF values: " + link.getAttribute("href"));
+//        }
+
+
+    }
+}
