395764e05f012dca72e7cd1e6ac9f1b29e2d0606
vupha <vupham129@gmail.com>
2022-02-15 09:27:15 -0800

    day5

---
+++ src/test/java/com/cydeo/tests/day5_testNG_intro_dropdowns/T2_RadioButtonHandling.java
@@ -0,0 +1,36 @@
+package com.cydeo.tests.day5_testNG_intro_dropdowns;
+
+import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.concurrent.TimeUnit;
+
+public class T2_RadioButtonHandling {
+    public static void main(String[] args) {
+        //TC #2: Radiobutton handling
+        //1. Open Chrome browser
+        WebDriver driver = WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+        //2. Go to https://practice.cydeo.com/radio_buttons
+        driver.get("https://practice.cydeo.com/radio_buttons");
+
+        //3. Click to “Hockey” radio button
+       // WebElement hockeyBtn = driver.findElement(By.xpath("(//div//input[@name='sport'])[3]"));
+        WebElement hockeyBtn = driver.findElement(By.xpath("//input[@id='hockey']"));
+        hockeyBtn.click();
+
+        //4. Verify “Hockey” radio button is selected after clicking
+       // System.out.println("hockeyBtn.isSelected() = " + hockeyBtn.isSelected());//true
+        if(hockeyBtn.isSelected()){
+            System.out.println("Button is selected. Verification Passed");
+        }else{
+            System.out.println("Button is not selected. Verification Failed");
+        }
+
+        driver.close();
+    }
+}
