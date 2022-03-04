7a293b1d2884b8ac66ab18f1db193a703aa2b4ea
vupha <vupham129@gmail.com>
2022-03-04 11:43:16 -0800

    day8_webTables_config_reader

--- src/test/java/com/cydeo/tests/day7_webtables_utilities_javafaker/T3_LoginScenarios.java
+++ src/test/java/com/cydeo/tests/day7_webtables_utilities_javafaker/T3_LoginScenarios.java
@@ -1,25 +1,15 @@
 package com.cydeo.tests.day7_webtables_utilities_javafaker;
 
+import com.cydeo.tests.base.TestBase;
 import com.cydeo.utilities.BrowserUtils;
 import com.cydeo.utilities.CRM_Utilities;
-import com.cydeo.utilities.WebDriverFactory;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import java.util.concurrent.TimeUnit;
-
-public class T3_LoginScenarios {
+public class T3_LoginScenarios extends TestBase {
     //TC #3: Login scenario
-    public WebDriver driver;
-    @BeforeMethod
-    public void setupMethod(){
-        driver = WebDriverFactory.getDriver("chrome");
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-    }
+
     /*
     @DataProvider(name = "logInCredentials")
     public Object[][] provideData(){
