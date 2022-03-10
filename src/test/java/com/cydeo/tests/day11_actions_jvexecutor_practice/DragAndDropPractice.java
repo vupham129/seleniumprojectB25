a06b864fb2e30e39d6ff9364772759318a0f23f2
vupha <vupham129@gmail.com>
2022-03-10 10:01:56 -0800

    day11

--- src/test/java/com/cydeo/tests/day11_actions_jvexecutor_practice/DragAndDropPractice.java
+++ src/test/java/com/cydeo/tests/day11_actions_jvexecutor_practice/DragAndDropPractice.java
@@ -15,21 +15,22 @@ public class DragAndDropPractice {
     public void drag_and_drop_test(){
         //1. Go to https://demos.telerik.com/kendo-ui/dragdrop/index
         Driver.getDriver().get("https://demos.telerik.com/kendo-ui/dragdrop/index");
-        BrowserUtils.sleep(4);
         //Locate Accept Cookies button and click
         WebElement acceptCookiesBtn = Driver.getDriver().findElement(By.id("onetrust-accept-btn-handler"));
         acceptCookiesBtn.click();
+        BrowserUtils.sleep(2);
         //2. Drag and drop the small circle to bigger circle.
         WebElement smallCircle = Driver.getDriver().findElement(By.xpath("//div[@id='draggable']"));
         WebElement bigCircle = Driver.getDriver().findElement(By.xpath("//div[@id='droptarget']"));
         Actions actions = new Actions(Driver.getDriver());
         //actions.dragAndDrop(smallCircle,bigCircle).build().perform();
         actions.moveToElement(smallCircle).clickAndHold().pause(1000).release(bigCircle).perform();
+       // actions.clickAndHold(smallCircle).pause(2000).moveToElement(bigCircle).release().perform();
 
         //3. Assert:
         //-Text in big circle changed to: “You did great!”
         BrowserUtils.sleep(2);
-        String actualTextBigCircle = Driver.getDriver().findElement(By.xpath("//div[text()='You did great!']")).getText();
+        String actualTextBigCircle = bigCircle.getText();
         Assert.assertEquals(actualTextBigCircle, "You did great!");
 
         Driver.closeDriver();
