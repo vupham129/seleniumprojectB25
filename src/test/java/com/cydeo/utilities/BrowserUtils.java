fcd437cca05e16a5699739980a1ab88685443554
vupha <vupham129@gmail.com>
2022-03-14 15:50:38 -0700

    day13

--- src/test/java/com/cydeo/utilities/BrowserUtils.java
+++ src/test/java/com/cydeo/utilities/BrowserUtils.java
@@ -5,6 +5,9 @@ In this class only general utility methods that are not related to some specific
  */
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 
 import java.util.Set;
@@ -47,6 +50,14 @@ public class BrowserUtils {
 
     }
 
+    /*
+    Creating a utility method for ExplicitWait, so we don't have to repeat the lines
+     */
+    public static void waitForInvisibilityOf(WebElement webElement){
+        //Driver.getDriver().manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
+        WebDriverWait wait = new WebDriverWait(Driver.getDriver(), 10);
+        wait.until(ExpectedConditions.invisibilityOf(webElement));
+    }
 
 
 
